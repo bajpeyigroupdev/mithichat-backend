29111c7a0175d8787a5bf9df01ffad74a2906198
Bajpeyi Group <bajpeyigroup.1@gmail.com>
2026-07-23 20:50:05 +0530

    fix: add missing getAdminRechargeHistory import in adminRoutes to fix production server crash

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -7,6 +7,7 @@ import {
     updateAdminProfile,
     addCoinsToUser,
     addDiamondsToUser,
+    getAdminRechargeHistory,
 } from '../controllers/adminAuthController';
 import {
     getDashboardStats,
